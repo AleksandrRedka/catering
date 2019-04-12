76fd32db9ad1a29a7728e2acf46d60c3f0210ac1
Aleksandr Redka <Aleksandr Redka>
2019-04-12 17:43:57 +0300

    first commit

---
+++ assest/js/script.js
@@ -0,0 +1,12 @@
+// $(document).ready(
+//     (function () {
+//         $('.portfolio_slider').slick({
+//             dots:false,
+//             infinite: true,
+//             slidesToShow: 3,
+//             slidesToScroll: 1,
+//             }
+//
+//         )
+//     })()
+// );
