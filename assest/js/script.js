3a571daa2834fcff0af01abf6c22c8c44cecd384
Aleksandr Redka <Aleksandr Redka>
2019-05-16 16:49:01 +0300

    start create new View Sliders

--- assest/js/script.js
+++ assest/js/script.js
@@ -3,7 +3,7 @@ $(document).ready(
         // Portfolio Slider
         (function () {
             $('.portfolio_slider').slick({
-                dots:false,
+                dots: false,
                 infinite: true,
                 slidesToShow: 3,
                 slidesToScroll: 1,
@@ -36,7 +36,7 @@ $(document).ready(
         //Gallery Slider
         (function () {
             $('.gallery_slider').slick({
-                dots:false,
+                dots: false,
                 infinite: true,
                 slidesToShow: 4,
                 slidesToScroll: 1,
@@ -77,8 +77,11 @@ $(document).ready(
         (function () {
             $('.read-more-js').click(function () {
                 $('.about_us_wrapper_info_text').toggleClass('about_us_wrapper_info_text_open');
-                if($(this).text() === 'Читать...'){$(this).text('Cкрыть')}
-                else{$(this).text('Читать...')}
+                if ($(this).text() === 'Читать...') {
+                    $(this).text('Cкрыть')
+                } else {
+                    $(this).text('Читать...')
+                }
             })
         })();
         // backgroundColor header
@@ -86,9 +89,9 @@ $(document).ready(
             $(document).on('scroll', function () {
                 let lengthScroll = $(document).scrollTop();
                 if (lengthScroll > 150) {
-                    $('header').css('background-color','rgba(255,255,255,1)');
+                    $('header').css('background-color', 'rgba(255,255,255,1)');
                 } else {
-                    $('header').css('background-color','')
+                    $('header').css('background-color', '')
 
                 }
             });
@@ -96,53 +99,61 @@ $(document).ready(
         // pop-up-buy
         (function () {
             $('.buy-js').click(openShop);
+
             function openShop() {
                 $('.wrapper_pop_up_product').addClass('wrapper_pop_up_product_open');
             };
             $('.close-pop-up-js').click(closeShop);
+
             function closeShop() {
                 $('.wrapper_pop_up_product').removeClass('wrapper_pop_up_product_open')
             };
             $('.post-shop-js').click(postOrder);
+
             function postOrder(e) {
                 e.preventDefault();
-                $('body').css('overflow','hidden');
-                $('.pop_up_buy').css('display','none');
+                $('body').css('overflow', 'hidden');
+                $('.pop_up_buy').css('display', 'none');
                 $('.pop_up_thanks').addClass('pop_up_thanks_open');
                 setTimeout(function () {
-                    closeShop();
-                    $('body').css('overflow','auto');
-                    $('.pop_up_buy').css('display','block');
-                    $('.pop_up_thanks').removeClass('pop_up_thanks_open');
+                        closeShop();
+                        $('body').css('overflow', 'auto');
+                        $('.pop_up_buy').css('display', 'block');
+                        $('.pop_up_thanks').removeClass('pop_up_thanks_open');
                     }
-                    ,2000)
+                    , 2000)
             }
         })();
         // pop up call
         (function () {
             $('.form-call-js').click(openForm);
+
             function openForm() {
-                $('body').css('overflow','hidden');
+                $('body').css('overflow', 'hidden');
                 $('.wrapper_pop_up_call').addClass('wrapper_pop_up_call_open');
             }
+
             $('.close-form-call-js').click(closeForm);
+
             function closeForm() {
-                $('body').css('overflow','auto');
+                $('body').css('overflow', 'auto');
                 $('.wrapper_pop_up_call').removeClass('wrapper_pop_up_call_open');
             }
+
             $('.form-post-js').click(postForm);
+
             function postForm(e) {
                 e.preventDefault();
-                $('.pop_up_call_title').css('display','none');
-                $('.call_form').css('display','none');
+                $('.pop_up_call_title').css('display', 'none');
+                $('.call_form').css('display', 'none');
                 $('.form-thanks-js').addClass('form_thanks_open');
                 setTimeout(function () {
                         closeForm();
-                        $('.pop_up_call_title').css('display','block');
-                        $('.call_form').css('display','block');
+                        $('.pop_up_call_title').css('display', 'block');
+                        $('.call_form').css('display', 'block');
                         $('.form-thanks-js').removeClass('form_thanks_open');
                     }
-                    ,2000)
+                    , 2000)
             }
         })();
         // Slick slider Main
@@ -192,58 +203,66 @@ $(document).ready(
                     fade: false,
                 });
             }
-            if ($(window).width() > '767'){
-                $('.reviews_slider').slick({
-                    arrows: true,
-                    prevArrow: '<button class="slick-review slick-prev slick-reviews-prev"><img src="assest/img/svg/Symbol15–7.svg" alt=""></button>',
-                    nextArrow: '<button class="slick-review slick-next slick-reviews-next"><img src="assest/img/svg/Symbol15–7.svg" alt=""></button>',
-                    slidesToShow: 5,
-                    slidesToScroll: 1,
-                    centerMode: true,
-                    centerPadding: '40rem',
-                    responsive: [
-
-                        {
-                            breakpoint: 1024,
-                            settings: {
-                                arrows: true,
-                                prevArrow: '<button class="slick-review slick-prev slick-reviews-prev"><img src="assest/img/svg/Symbol15–7.svg" alt=""></button>',
-                                nextArrow: '<button class="slick-review slick-next slick-reviews-next"><img src="assest/img/svg/Symbol15–7.svg" alt=""></button>',
-                                slidesToShow: 2,
-                                slidesToScroll: 1,
-                                centerPadding: '28rem',
-                                centerMode: true,
-
-                            }
-                        },
-                ],
-                });
+            if ($(window).width() > '767') {
+                $('.reviews_slider').owlCarousel({
+                        nav: true,
+                        margin: 10,
+                        items: 4,
+                        loop: true,
+                        autoWidth: true,
+                        navText: [
+                            "<button class=\"review_change slick-prev slick-reviews-prev\"><img src=\"assest/img/svg/Symbol15–7.svg\" alt=\"\"></button>",
+                            "<button class=\"review_change slick-next slick-reviews-next\"><img src=\"assest/img/svg/Symbol15–7.svg\" alt=\"\"></button>"
+                        ],
+                    }
+                )
             }
-                })();
+        })();
+        //Select Active class in Reviews Slider
+        (function () {
+            function addClassActive(){
+                let slidesActive = $('.reviews_slider').find('div.active:first');
+                slidesActive.addClass('review_active');
+            }
+            addClassActive();
+            // owl = $('#owl-carousel');
+            // owl.owlCarousel({onInitialized: carouselInitialized});
+            // function carouselInitialized(event) {
+                $('.review_change').click(function () {
+                    $('.review_active').removeClass('review_active');
+                    $('.reviews_slider').find('.owl-stage').css('left','16vw');
+                    addClassActive();
+                });
+            // }
+        })();
         // pop-up-calc
         (function () {
             $('.calc-js').click(calcEvent);
+
             function calcEvent() {
                 $('.wrapper_pop_up_calc').addClass('wrapper_pop_up_calc_open');
                 // $('.main_wrapper').addClass('main_wrapper_open');
-                $('body').css('overflow','hidden');
+                $('body').css('overflow', 'hidden');
             }
+
             $('.close-calc-js').click(closeCalc);
+
             function closeCalc() {
                 $('.wrapper_pop_up_calc').removeClass('wrapper_pop_up_calc_open');
                 // $('.main_wrapper').css('display','block');
-                $('body').css('overflow','auto');
+                $('body').css('overflow', 'auto');
             }
         })();
         //input range on pop-up-calc
-        (function() {
-            $('#quantity').on('input',range);
+        (function () {
+            $('#quantity').on('input', range);
+
             function range() {
                 let maxValue = $('#quantity').attr('max');
                 let value = $('#quantity').val();
                 let val = (value / maxValue * 100);
-                $('#quantity').css('background','-webkit-linear-gradient(left,#00A651 0%,#00A651 '+val+'%,#EBEBEB  '+val+'%, #EBEBEB 100% )');
-                $('.calc_quantity_check_number').html('<p>'+value+'/<span>'+maxValue+'</span></p>')
+                $('#quantity').css('background', '-webkit-linear-gradient(left,#00A651 0%,#00A651 ' + val + '%,#EBEBEB  ' + val + '%, #EBEBEB 100% )');
+                $('.calc_quantity_check_number').html('<p>' + value + '/<span>' + maxValue + '</span></p>')
             }
         })();
         // Slider select Categories
@@ -252,11 +271,11 @@ $(document).ready(
         // Calc-Form
         (function () {
             $('.full-price-next-js').click(function () {
-                    $('.wrapper_calc_form').css('right','0%')
+                    $('.wrapper_calc_form').css('right', '0%')
                 }
             );
             $('.full-price-return-js').click(function () {
-                $('.wrapper_calc_form').css('right','-100%')
+                $('.wrapper_calc_form').css('right', '-100%')
             })
         })()
     })()
